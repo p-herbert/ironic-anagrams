f04fea53710eecd98cf0c191c53c7f775ea3f42f
p-herbert <pete.a.herbert@gmail.com>
2016-09-13 20:31:04 -0700

    Add textbox for comments and test data

--- client/journalapp/app/Comment_Components/CommentsScene.js
+++ client/journalapp/app/Comment_Components/CommentsScene.js
@@ -14,6 +14,18 @@ import {
 import CommentList from './CommentList';
 import styles from '../styles/MessageSceneStyles';
 
+var data = [{text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'},
+  {text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'}];
+
 export default class CommentsScene extends Component {
   constructor(props) {
     super(props);
@@ -21,16 +33,32 @@ export default class CommentsScene extends Component {
 
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      testData: ds.cloneWithRows([{text: 'Hello World', location: 'Bay Area', createdAt: '2016-09-13T21:45:11.328Z', username: 'Pete'}])
+      testData: ds.cloneWithRows(data),
+      dynamicHeight: () => { return {height: Dimensions.get('window').height - 49 - 500}}
     };
-    
+
   }
 
   render() {
     return (
-      <ScrollView style={ styles.container } ref='scrollView'>
+      <View style={styles.container}>
+        <View style={
+          {marginTop: 0,
+            marginLeft: 0,
+            borderBottomWidth: 0.5,
+            borderColor: '#cccccc',
+            paddingBottom: 16}
+        }>
+          <TextInput
+              keyboardType='default'
+              keyboardAppearance='light'
+              multiline={ true }
+              placeholder= 'Comment...'
+              style={ [this.state.dynamicHeight(), styles.bodyWidth, styles.fadedText] }
+              maxLength={ 100 }/>
+        </View>
         <CommentList entries={this.state.testData}/>
-      </ScrollView>
+      </View>
     )
   }
 }
