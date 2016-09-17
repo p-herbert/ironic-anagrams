fde988fb7cdc7f0bc7ec0e7245dd8145a64d412f
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-09-17 15:32:47 -0700

    Fix styles

--- client/journalapp/app/styles/EntryListStyles.js
+++ client/journalapp/app/styles/EntryListStyles.js
@@ -6,10 +6,27 @@ import {
 // marginBottom allows space for the lower navbar
 const styles = StyleSheet.create({
   container: {
-    position:'relative',
-    paddingTop:10, 
-    marginBottom: 37.5
+    position: 'relative',
+    paddingTop: 10, 
+    marginBottom: 37.5,
+    opacity: 1
+  },
+  hiddenRowContainer: {
+    borderWidth: 0, 
+    height: 90, 
+    width: Dimensions.get('window').width, 
+    overflow: 'hidden',
+    alignSelf: 'flex-start', 
+    backgroundColor: 'red'
+  },
+  hiddenRow: {
+    paddingTop: 30,
+    paddingLeft: 35,
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: 'white'
   }
-})
+
+});
 
 module.exports = styles;
