881114afc00d81bf7846d3abf956180f6c1e868e
p-herbert <pete.a.herbert@gmail.com>
2016-09-19 22:45:32 -0700

    Pass function getEntries

--- client/journalapp/app/Entry_Components/EntriesTab.js
+++ client/journalapp/app/Entry_Components/EntriesTab.js
@@ -46,7 +46,7 @@ export default class EntriesTab extends Component {
               keyboardAppearance='light'
               placeholder= 'Search tags'
               style={ {height: 50, textAlign: 'center', borderWidth: 1, borderColor: '#cccccc', marginBottom: 16, backgroundColor: '#f9ebc3'} }
-              onChangeText={ (text) => this.props.filterTags(text) }/>
+              onChangeText={ (text) => this.props.filterTags(text, this.props.getEntries) }/>
           <Text style={ styles.date }>{ DateFormatter(new Date(), 'ddd, mmm d') }</Text>
           <Button style={ styles.headerButton } 
                   onPress={ () => this.props.navigator.push({ title: 'MessageScene'}) }>
